5bb84e2e0d2190383177f9d24e258afddbabe0a0
promolina <connect@promolina.com>
2025-01-15 07:57:17 -0800

    updated accessibility in navbar

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const navbarItems = document.querySelector('.navbar-items');
 
+const menuButton = document.querySelector('.hamburger-menu');
+menuButton.addEventListener('click', () => {
+  const expanded = menuButton.getAttribute('aria-expanded') === 'true';
+  menuButton.setAttribute('aria-expanded', !expanded);
+});
+
+
 hamburger.addEventListener('click', () => {
   // Toggle active state for the hamburger menu animation
   hamburger.classList.toggle('active');
